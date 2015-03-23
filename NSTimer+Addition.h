01a0ce1c59a9502a0117f1680d57a25498c4e18f
AndreaArlex <arlexvincy@163.com>
2015-03-23 14:18:42 +0800

    CycleScrollView 循环滚动scrollView
    CycleScrollView 循环滚动scrollView

---
+++ NSTimer+Addition.h
@@ -0,0 +1,11 @@
+//
+
+
+#import <Foundation/Foundation.h>
+
+@interface NSTimer (Addition)
+
+- (void)pauseTimer;
+- (void)resumeTimer;
+- (void)resumeTimerAfterTimeInterval:(NSTimeInterval)interval;
+@end
